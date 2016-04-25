aa37f7adec9d2cd1a8146c49df489bbee919839b
1417777 <e.w.koloh@rgu.ac.uk>
2016-04-25 22:27:40 +0100

    Final Commit 1

---
+++ Week7Database/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1417777
+ * Date: 25/04/2016
+ * Time: 22:22
+ */
+define('DB_ap-cdbr-azure-east-c.cloudapp.net', 'your-azure-server');
+define('DB_b6d74ce4980e5b', 'your-username');
+define('DB_f19b9429', 'your-password');
+define('DB_rgu1417777', 'your-database');
+$db = mysqli_connect(DB_ap-cdbr-azure-east-c.cloudapp.net,DB_b6d74ce4980e5b,DB_f19b9429,DB_rgu1417777);
+?>
