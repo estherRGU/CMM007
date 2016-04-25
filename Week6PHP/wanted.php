84e17c16d6d7f54f59c09b1db264c93e50e0a721
1417777 <e.w.koloh@rgu.ac.uk>
2016-04-25 10:10:45 +0100

    Final Commit 1

---
+++ Week6PHP/wanted.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1417777
+ * Date: 25/04/2016
+ * Time: 10:09
+ *
+ *
+ *
+ * The people of Aberdeen are still struggling to understand at what age they are allowed to have specs, mugs, and sausage rolls. Make a webpage that stores a variable called $wantedgood. Create a switch statement that takes this variable and prints out how old you have to be.
+E.g. if $wantedgood = “mugs” then the system would print “You have to be 18 to buy mugs”.
+ */
+
+$wantedgood = "mugs";
+
+switch ($wantedgood) {
+    case "specs":
+        echo "You have to be 16 to buy specs";
+        break;
+    case "mugs":
+        echo "You have to be 18 to buy mugs";
+        break;
+    case "sausage rolls":
+        echo "You have to be 21 to buy sausage rolls";
+        break;
+    default:
+        echo "You haven't selected a thing to buy";
+}
+
